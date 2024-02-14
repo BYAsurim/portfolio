2aeb2f1458b637b7a708e0d3c685816f7797c33d
BYAsurim <Pers0naL276276@gmail.com>
2024-02-14 10:19:20 +0300

    add styles for Header.tsx

---
+++ src/components/Link.ts
@@ -0,0 +1,31 @@
+import {theme} from "../styles/Theme";
+import styled from "styled-components";
+
+export const Link = styled.a `
+    font-weight: 400;
+    font-size: 14px;
+    letter-spacing: 1px;
+    text-transform: uppercase;
+    padding:10px;
+    position: relative;
+    z-index: 0;
+    
+    &::before {
+        content: '';
+        display: inline-block;
+        width: 100%;
+        background-color: ${theme.colors.accent};
+
+        position: absolute;
+        bottom: 5px;
+        left: 0;
+        z-index: -1;
+    }
+
+
+    &:hover{
+        &::before{
+            height: 10px;
+        }
+    }
+`
